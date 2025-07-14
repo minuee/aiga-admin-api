4b858839d889c1723e0a886788b202bfa4d555db
nohsn <minuee@kormedi.com>
2025-07-14 17:46:25 +0900

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Res, NotFoundException } from '@nestjs/common';
+import { Controller, Query, Get, Post, Delete, Body, Res, NotFoundException } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Response } from 'express';
 const webPush  =  require('web-push')
@@ -27,6 +27,7 @@ export class AppController {
       const { session_id,user_id, user_email,user_agent, user_message,error_message, error_code } = body;
       if ( session_id != null ) {
         // SQL 쿼리 실행
+        const timestamp = new Date(Date.now() + 9 * 60 * 60 * 1000).toISOString().slice(0, 19).replace('T', ' ');
         await this.dataSource.query(
           `
           INSERT INTO errorlog (
@@ -36,8 +37,9 @@ export class AppController {
             user_agent,
             user_message,
             error_message,
-            error_code
-          ) VALUES (?, ?, ?, ?, ?, ?, ?)
+            error_code,
+            create_dt
+          ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
           `,
           [
             user_id || null,
@@ -47,6 +49,7 @@ export class AppController {
             user_message || null,
             error_message || null,
             error_code || null,
+            timestamp || null,
           ],
         );
         res.status(201).json({ success: true ,msg:'등록완료'});
@@ -61,6 +64,66 @@ export class AppController {
     }
   }
 
+  @Post('subscribe')
+  async registSubscribe(@Body() body: any, @Res() res: Response) {
+    try {
+      const { user_id, user_email,user_agent, endpoint,scribe_key } = body;
+      if ( endpoint != null ) {
+        // SQL 쿼리 실행
+        const timestamp = new Date(Date.now() + 9 * 60 * 60 * 1000).toISOString().slice(0, 19).replace('T', ' ');
+        await this.dataSource.query(
+          `
+          INSERT INTO pwalog (
+            user_id,
+            user_email,
+            user_agent,
+            endpoint,
+            scribe_key,
+            create_dt
+          ) VALUES (?, ?, ?, ?, ?, ?)
+          `,
+          [
+            user_id || null,
+            user_email || null,
+            user_agent || null,
+            endpoint || null,
+            scribe_key || null,
+            timestamp || null,
+          ],
+        );
+        res.status(201).json({ success: true ,msg:'등록완료'});
+      }else{
+        res.status(201).json({ success: true , msg:"값이 없음" });
+      }
+    } catch (error) {
+      console.error("로그 저장 오류:", error);
+      throw new NotFoundException('로그 저장 실패');
+    }
+  }
+
+  @Delete('subscribe')
+  async removeSubscribe(@Query('key') endpoint: string, @Res() res: Response) {
+    try {
+      if (endpoint) {
+  
+        await this.dataSource.query(
+          `
+          DELETE FROM pwalog
+          WHERE endpoint = ?
+          `,
+          [endpoint],
+        );
+  
+        res.status(200).json({ success: true, msg: '삭제 완료' });
+      } else {
+        res.status(400).json({ success: false, msg: 'endpoint 값이 없습니다.' });
+      }
+    } catch (error) {
+      console.error('삭제 오류:', error);
+      throw new NotFoundException('삭제 실패');
+    }
+  }
+
   @Post('send-notification')
   receiveSendPush(@Body() req: any, @Res() res: Response) {
     res.setHeader('Content-Type', 'text/event-stream');
@@ -69,19 +132,24 @@ export class AppController {
     webPush.setVapidDetails('mailto:minuee@kormedi.com', publicVapidKey, privateVapidKey);
     const subscriptions = [
       {
-        endpoint: "https://fcm.googleapis.com/fcm/send/fK8yGm7SOcg:APA91bGlo_czrvHk2XmCPASyxUMrvZEQ3yg9Ntn2Fx0zfGNrydL0HnYb3Kv4eXWxLKD8wXf6KqIrxRpL9p2C2T6Y-FfBWMwlhNQsnEjCeGfCWueayjQg7_3wUickLNo86-Ld0tUVTRX3",
-        keys: {
-          p256dh: "BOOuyzc9CuEQf26mLhwioh6b2G546O2zAk6sHu2QhQQR-g3xv5mU0N4qkAcSCB3RTk_Qu4sRAvPrMir045E2Vm0",
-          auth: "EP9KTfLfYIVPRMvQ3BvWDg"
+        "endpoint":"https://fcm.googleapis.com/fcm/send/fm2hmlT7kQc:APA91bEec-rldnEMPg8p1VROJc9uH7iRiGyc8PYpS3sbjYwOnslPVpxsS3zQdG5vGaeRY5emg25Q2x-JJZoaw1VwK-jFk_jUM-L8y4OzC2jf4Pwlv_GsTRx2hJ2-f_AGCkYmbHu33Fcc","expirationTime":null,
+        "keys":{"p256dh":"BLP9eodLTiR-bocEKK7QfcY48aY6gF8wD3IS0T31KaDTUEnF7ovM6S__y2fTDTcPeF01yUk02r9Iw5T-sIN1N6E","auth":"hlAOjAgd-RY7VfBVVvNigQ"}
+      },
+      {
+        "endpoint": "https://jmt17.google.com/fcm/send/dzOvj0_24MY:APA91bFyLgMsl2_1lH5Z9j7H332RKjx0l-3jXdejY6lRFWKj588EJL5sIzxqADfTvpzAYGe_SXo7wLUejo1BIQCdNYsYcjx_plgU4axPwL-CZEar_uCdmtlunK_9SnHe0cMf9yLyc0oL",
+        "expirationTime": null,
+        "keys": {
+            "p256dh": "BGY1vPeZs_inj8-BOvkkj0B_oxzGq0Yz1U3ZhPT244nEu-24fMfVNL00PtVbUoFqsVKXao1LrECxIR7mLBfGwhU",
+            "auth": "fGKiWrW45OEtuLJ_AicVjg"
         }
       },
       {
-        endpoint: 'https://web.push.apple.com/QLs8VncrWUY3UG5eDKv3oEcLzq-jY7RAedXdEd7b_QaQVFKf7o6VHOZ6MBm4FPE5hJgnrNDhBVB3gvqIFHUSIJFZBs113k-7gzaishnNtpyGYnGwOHZLrROGm_Ns-AnBQBezq4mqYU2rZijyU_L1khzeqpkDqAc6FJBhzi_7ixc',
-        keys: {
-          p256dh: 'BFcAmF2usUkyqcNy-ijYWCbbkH7tXnSyaYFdUIrw7guHlkSCza9BVbl4j9FyvTB2gJhYmqHdG98E2B2jUgVvN8M',
-          auth: 'UB4nmePBFAtABwdqlDFecQ'
+        "endpoint":"https://web.push.apple.com/QFr87S555VSGrHFELyZEgRaV-PwtNTWavloQnibgibIvGjgEINYkm0xTcMHPSqeVwHIq5oPHCsZimc0WPw_iKa0GzYwEk-Tz1zcBcGO6kNuEeO2OffvZMQ_TssCKqtkXMbaYQAoxfbaXtRiepUk9b3I9vGykE36-PAPzjp2xiFQ",
+        "keys":{
+          "p256dh":"BACuKPYHdfIqzucW6eWUSMhOgzC6YVKMKPMYQTxv8EM3uR2yl0Dvsd-CtElKOggk_OYVGLK8KkIn4QvNrizDnSI",
+          "auth":"aHvOxmUls-aOITmxotnATg"
         }
-      }      
+      }
     ]
     
     try {
@@ -94,7 +162,7 @@ export class AppController {
         icon: '/img/fav/Icon-196.png',
         badge: '/img/fav/Icon-72.png',
         data: {
-          url: 'https://aigadev.kormedi.com',
+          url: 'https://aigadev.kormedi.com/ko/chat',
         },
       });
 
@@ -207,3 +275,4 @@ export class AppController {
     }
   }
 }
+
